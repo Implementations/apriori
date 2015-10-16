53eb3922aefdca866a06a2bd2297dd76ef4d3f36
Kanishk Tripathi <kanishk.tripathi87@gmail.com>
2015-10-15 21:50:31 -0400

    Discretize continous data

--- Apriori/src/edu/rochester/kanishk/Transaction.java
+++ Apriori/src/edu/rochester/kanishk/Transaction.java
@@ -22,13 +22,15 @@ public class Transaction {
 	public void cleanUp(float gain, float loss, Map<Item, Integer> oneItemSet) {
 		correctValue(gain, itemList.get(Constants.GAIN_INDEX));
 		correctValue(loss, itemList.get(Constants.LOSS_INDEX));
+		ageGroup(itemList.get(Constants.AGE_INDEX));
+		hoursGroup(itemList.get(Constants.HOURS_INDEX));
 		List<Item> newList = new ArrayList<>(itemList.size());
 		Integer itemCount;
-		for(Item i : itemList) {
-			if(!Constants.GARBAGE.equals(i.value)) {
+		for (Item i : itemList) {
+			if (!Constants.GARBAGE.equals(i.value)) {
 				newList.add(i);
 				itemCount = oneItemSet.get(i);
-				if(itemCount == null) {
+				if (itemCount == null) {
 					oneItemSet.put(i, 1);
 				} else {
 					itemCount++;
@@ -48,6 +50,36 @@ public class Transaction {
 				item.itemType = Constants.LOW;
 			} else {
 				item.itemType = Constants.HIGH;
+			}
+		}
+	}
+
+	private void ageGroup(Item item) {
+		if (!Constants.GARBAGE.equals(item.value)) {
+			int value = Integer.parseInt(item.value);
+			if (value <= 25) {
+				item.itemType = Constants.YOUTH;
+			} else if (value <= 45) {
+				item.itemType = Constants.MIDDLE_AGE;
+			} else if (value < 65) {
+				item.itemType = Constants.SENIOR;
+			} else {
+				item.itemType = Constants.SUPER_SENIOR;
+			}
+		}
+	}
+
+	private void hoursGroup(Item item) {
+		if (!Constants.GARBAGE.equals(item.value)) {
+			int value = Integer.parseInt(item.value);
+			if (value <= 25) {
+				item.itemType = Constants.PART_TIME;
+			} else if (value <= 40) {
+				item.itemType = Constants.FULL_TIME;
+			} else if (value < 60) {
+				item.itemType = Constants.OVERTIME;
+			} else {
+				item.itemType = Constants.BURNOUT;
 			}
 		}
 	}
